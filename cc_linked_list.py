a5ab550bc67182dea63d77d6808071abcd5c3659
Chako <ryland0000@gmail.com>
2023-08-06 06:20:32 -0700

    prepend linked list

---
+++ cc_linked_list.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None  # no nodes and so head will be set to a value of None
+
+    def append(self, value):
+        # this new_node is floating out in space and not yet connected/docked to another Node
+        new_node = Node(value)
+
+        if self.head is None:  # if there is None value attached to the head
+            self.head = new_node  # now we are creating a head since we now have 2 nodes
+            print("Head Node created:", self.head.value)
+            return
+
+        node = self.head
+        # traverse to the node.next that has a None value (till we reach the tail node)
+        while node.next is not None:
+            node = node.next
+
+        node.next = new_node  # new_node is the new tail
+        # node.next.value is the value of the tail
+        print("Appended new Node with value:", node.next.value)
+
+    def prepend(self, value):
+        new = Node(value)
+
+        if self.head is None:  # if there is None value attached to the head
+            self.head = new  # now we are creating a head since we now have 2 nodes
+            print("Head Node created:", self.head.value)
+            return
+
+        else:
+            old = self.head
+            self.head = new
+            self.head.next = old
+
+        print("Prepend new Head Node with value", new.value)
+        print("Node following Head is:", old.value)
+
+
+llist = LinkedList()
+llist.prepend("First Node")
+llist.prepend("Inserted New First Node")
