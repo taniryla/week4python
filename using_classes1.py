c2dd4fad1fd1bdf7ccf9f9395e5aab8a710586b0
Chako <ryland0000@gmail.com>
2023-08-06 02:48:03 -0700

    constructor method

--- using_classes1.py
+++ using_classes1.py
@@ -23,3 +23,15 @@ class Player:
         self.name = name
         self.hp = hp
         self.score = 0
+
+
+player1 = Player("Aaron", 1200)
+player2 = Player("Irene", 1300)
+
+print("P1:", player1.name, " -- HP:", player1.hp, "-- SCORE: ", player1.score)
+print("P2:", player2.name, " -- HP:", player2.hp, "-- SCORE: ", player2.score)
+
+player1.hp += 500
+player1.score += 10
+print("P1:", player1.name, " -- HP:", player1.hp, "-- SCORE: ", player1.score)
+print("P2:", player2.name, " -- HP:", player2.hp, "-- SCORE: ", player2.score)
