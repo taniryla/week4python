48ce10e2d12f8e8f22a5a1706b182d5850eca2a8
Chako <ryland0000@gmail.com>
2023-08-06 01:55:49 -0700

    simple class

--- using_classes1.py
+++ using_classes1.py
@@ -1,2 +1,8 @@
-class Player:
-    max_hp = 4000
+class Player:  # this creates a simple class with the name Player
+    max_hp = 4000  # max_hp is a class attribute
+
+
+player1 = Player()
+print(player1.max_hp)
+player2 = Player()
+print(player2.max_hp)
